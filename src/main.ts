af9a22db3d2d2f1ba4a10f140883917f14ba9a2b
Freddy <45133628+anonimo10@users.noreply.github.com>
2026-05-23 16:21:59 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,17 +6,23 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api/v2');
-   app.useGlobalPipes(
+
+  app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
       transform: true,
       transformOptions: {
-        enableImplicitConversion:true
-      }
+        enableImplicitConversion: true,
+      },
     }),
   );
-  await app.listen(process.env.PORT ?? 3000);
-  console.log(`App running on port ${process.env.PORT} `);
+
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port, '0.0.0.0');
+
+  console.log(`App running on port ${port}`);
 }
+
 bootstrap();
